31b45c670635ab2ce6f87ac41902fb3809f8dbef
Agampreet <bajajagampreet@gmail.com>
2025-10-22 16:45:17 -0400

    Created env and hid sql password

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -1,8 +1,12 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 class conf:
     db_host = "localhost"
     db_name = "sandwich_maker_api"
     db_port = 3306
     db_user = "root"
-    db_password = "rootroot"
+    db_password = os.getenv("DB_PASSWORD", "")
     app_host = "localhost"
     app_port = 8000
